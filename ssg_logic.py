4ab98c5ad7bee3a82fb3e29e7d781526bf19f36e
nareshix <nareshix66@gmail.com>
2025-05-26 17:35:17 +0800

    combined 2 py file into 1

--- ssg_logic.py
+++ ssg_logic.py
@@ -15,6 +15,28 @@ import datetime
 from header_config import setup_header_in_layout_html
 
 
+import yaml
+
+def setup_header_in_layout_html(): 
+    with open("header_config.yaml", "r") as f:
+        config = yaml.safe_load(f)
+    github_link  = config['github_link']
+    dropdowns = config['dropdowns']
+    internals = config['internals']
+    externals = config['externals']
+    env = Environment(loader=FileSystemLoader('templates'))
+    template = env.get_template('layout_no_header.html')
+    rendered = template.render(
+        dropdowns=dropdowns,
+        internals=internals,
+        externals=externals,
+        github_link=github_link,
+    )
+
+    with open('templates/layout.html', 'w') as f:
+        f.write(rendered)
+
+
 def generate_slug(text_to_slugify):
     text = str(text_to_slugify).lower()
     text = re.sub(r'[^\w\s-]', '', text)
