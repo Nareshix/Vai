f0e8b3d4585489ce50bd5ae41882107a0469179c
nareshix <nareshix66@gmail.com>
2025-05-27 21:48:33 +0800

    made the a href search and repalce algo more robust

--- vai/main.py
+++ vai/main.py
@@ -655,13 +655,19 @@ def add_github_prefix_to_static_resources(html, github_repo_name):
     for tag_name, attr in tags_attrs.items():
         for tag in soup.find_all(tag_name):
             if tag.has_attr(attr):
+                # For <a> tags, skip if target="_blank"
                 if tag.name == 'a' and tag.get('target') == '_blank':
                     continue
-
-                val = tag[attr]
-                if '/static' in val and not val.startswith(f'{github_repo_name}/static'):
-                    new_val = val.replace('/static', f'{github_repo_name}/static')
-                    tag[attr] = new_val
+                elif tag.name == 'a':
+                    val = tag[attr]
+                    if val.startswith('/') and not val.startswith(github_repo_name):
+                        new_val = f'{github_repo_name.rstrip("/")}{val}'
+                        tag[attr] = new_val
+                else:                        
+                    val = tag[attr]
+                    if '/static' in val and not val.startswith(f'{github_repo_name}/static'):
+                        new_val = val.replace('/static', f'{github_repo_name}/static')
+                        tag[attr] = new_val
 
     updated_html = str(soup)
     return updated_html
