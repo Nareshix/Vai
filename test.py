25a29f73359076c170e0226ba17f635b8b4abf61
nareshix <nareshix66@gmail.com>
2025-05-27 21:48:51 +0800

    test

--- test.py
+++ test.py
@@ -16,24 +16,23 @@ def add_github_prefix_to_static_resources(html, github_repo_name):
                 # For <a> tags, skip if target="_blank"
                 if tag.name == 'a' and tag.get('target') == '_blank':
                     continue
-
-                val = tag[attr]
-                if '/static' in val and not val.startswith(f'{github_repo_name}/static'):
-                    new_val = val.replace('/static', f'{github_repo_name}/static')
-                    tag[attr] = new_val
+                elif tag.name == 'a':
+                    val = tag[attr]
+                    if val.startswith('/') and not val.startswith(github_repo_name):
+                        new_val = f'{github_repo_name.rstrip("/")}{val}'
+                        tag[attr] = new_val
+                else:                        
+                    val = tag[attr]
+                    if '/static' in val and not val.startswith(f'{github_repo_name}/static'):
+                        new_val = val.replace('/static', f'{github_repo_name}/static')
+                        tag[attr] = new_val
 
     updated_html = str(soup)
     return updated_html
 
 # Sample test
 html = '''
-<a href="/static/file1.pdf">Download</a>
-<a href="/static/file2.pdf" target="_blank">Open in new tab</a>
-<link rel="icon" href="/staticfavicon.png" />
-<link rel="stylesheet" href="/static/style.css"></head>
-<script src="/static/script.js" defer></script>
-<img src="/static/logo.png" alt="logo" width="38">
-<img src="/static/logo.png" alt="logo" width="64">
+<a class=sidebar-link href=/asd/wtr/>wtr</a>    
 '''
 
-print(add_github_prefix_to_static_resources(html, '/vai'))
+print(add_github_prefix_to_static_resources(html, '/vai/'))
